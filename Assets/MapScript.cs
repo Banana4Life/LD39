4590b0910490f455c8ae06df2843e21a06c00b1f
Anselm Brehme <development.anselm@breh.me>
2017-07-29 13:49:28 +0200

    Update Navmesh at runtime

---
+++ Assets/MapScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MapScript : MonoBehaviour
+{
+	public bool updateMesh;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (updateMesh)
+		{
+			var navMeshSurface = GetComponent<NavMeshSurface>();
+			navMeshSurface.BuildNavMesh();
+			updateMesh = false;
+		}
+	}
+}
