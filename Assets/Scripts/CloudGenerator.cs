673c53ea3e0bfcef6f1ea4f357e08a6e3af12e66
Anselm Brehme <development.anselm@breh.me>
2017-07-29 19:12:15 +0200

    stuff and things

---
+++ Assets/Scripts/CloudGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudGenerator : MonoBehaviour {
+
+	public Texture2D cloudMap;
+	public GameObject cloud;
+	
+	// Use this for initialization
+	void Start () {
+		generateClouds(gameObject.transform.parent.gameObject);
+	}
+	
+	public void generateClouds(GameObject map)
+	{
+		var distScript = map.GetComponentInChildren<RandomDistributionScript>();
+		int i = 0;
+		for (var x = 0; x < distScript.bounds.x; x+=2)
+		{
+			for (var z = 0; z < distScript.bounds.z; z+=2)
+			{
+				if (hasClouds(new Vector2(x, z), cloudMap, map))
+				{
+					var aCloud = Instantiate(cloud, gameObject.transform);
+					aCloud.transform.localPosition = new Vector3(x, 2, z);
+					aCloud.name = "Cloud " + x + ":" + z + "(" + i++ + ")";
+				} 
+			}
+		}
+	}
+
+	private static bool hasClouds(Vector2 mapPos, Texture2D clouds, GameObject map)
+	{
+		var distScript = map.GetComponentInChildren<RandomDistributionScript>();
+		int x = Mathf.FloorToInt(mapPos.x / distScript.bounds.x * clouds.width);
+		int z = Mathf.FloorToInt(mapPos.y / distScript.bounds.z * clouds.height);
+		var pixel = clouds.GetPixel(x, z);
+		return pixel.a > 0;
+	}
+}
---
+++ Assets/Scripts/MapScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MapScript : MonoBehaviour
+{
+	public bool updateMesh;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (updateMesh)
+		{
+			var navMeshSurface = GetComponent<NavMeshSurface>();
+			navMeshSurface.BuildNavMesh();
+			updateMesh = false;
+		}
+	}
+}
