8899ef66d9af2676f70231e72946196f581b3212
Anselm Brehme <development.anselm@breh.me>
2017-07-29 23:34:38 +0200

    cam

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 	public GameObject player;
-	public float height = 8;
 
 	// Update is called once per frame
 	void Update ()
 	{
 		var playerPos = player.transform.position;
-		transform.position = new Vector3(playerPos.x, playerPos.y + height, playerPos.z);
+		transform.position = new Vector3(playerPos.x, playerPos.y + 10, playerPos.z);
 	}
 }
