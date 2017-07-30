80811801929ab4733ef5968e4aec396163d9a66b
Phillip Schichtel <phillip.public@schich.tel>
2017-07-30 11:22:14 +0200

    remove blocking asteroids

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -33,8 +34,38 @@ public class EnemyController : MonoBehaviour
         {
             flipTarget(agent);
         }
+        RemoveBlockingAsteroids();
     }
-    
+
+    private void RemoveBlockingAsteroids()
+    {
+        const int asteroidArea = 1 << 3;
+        const int asteroidLayer = 1 << 8;
+        NavMeshHit navMeshHit;
+        if (NavMesh.SamplePosition(new Vector3(transform.position.x, 0, transform.position.z), out navMeshHit, 1, asteroidArea))
+        {
+//            Debug.LogWarning("Is in Asteroid Area");
+            var rb = GetComponent<Rigidbody>();
+            if (rb.velocity.sqrMagnitude > 0)
+            {
+//                Debug.LogWarning("Is moving");
+                var pos = transform.position;
+                pos.y = 1;
+                RaycastHit sphereHit;
+                if (Physics.SphereCast(pos, 1f, transform.forward, out sphereHit, 5, asteroidLayer))
+                {
+//                    Debug.LogWarning("Has asteroid near by");
+                    Destroy(sphereHit.collider.gameObject);
+                    GameObject.Find("Map").GetComponent<MapScript>().UpdateMesh();
+                }
+                else
+                {
+//                    Debug.LogWarning("No asteroid!!!!");
+                }
+            }
+        }
+    }
+
     public static bool DidAgentReachDestination(NavMeshAgent agent)
     {
         var distance = Vector3.Distance(agent.gameObject.transform.position, agent.destination);
