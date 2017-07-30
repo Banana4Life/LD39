366287ecf6d9566a457974932a79dc7d492f0454
Phillip Schichtel <phillip.public@schich.tel>
2017-07-30 21:53:38 +0200

    make clouds transparent instead of culling them

---
+++ Assets/Scripts/CloudModifier.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class CloudModifier : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other)
+	{
+		changeAlpha(other.gameObject, .1f);
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		changeAlpha(other.gameObject, 1f);
+	}
+
+	private static void changeAlpha(GameObject obj, float alpha)
+	{
+		var emitter = obj.GetComponent<ParticleSystem>();
+		var main = emitter.main;
+		var startColor = main.startColor;
+		var color = startColor.color;
+		color.a = alpha;
+		startColor.color = color;
+		main.startColor = startColor;
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -99,4 +99,5 @@ public class PlayerController : MonoBehaviour
 		point.y = transform.position.y;
 		transform.LookAt(point);
 	}
+	
 }
