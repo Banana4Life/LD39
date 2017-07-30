cadd17a9e7ba582792e4fd5cc4988ac0c812d367
Phillip Schichtel <phillip.public@schich.tel>
2017-07-30 20:44:21 +0200

    show effect when in clouds

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PostProcessing;
 
 public class CameraController : MonoBehaviour
 {
 	public GameObject player;
+	private PostProcessingBehaviour postProcessingBehaviour;
 
-	// Update is called once per frame
+	private void Start()
+	{
+		postProcessingBehaviour = GetComponent<PostProcessingBehaviour>();
+		postProcessingBehaviour.enabled = true;
+	}
+
 	void Update ()
 	{
 		var playerPos = player.transform.position;
 		transform.position = new Vector3(playerPos.x, playerPos.y + 10, playerPos.z);
+		var playerController = player.GetComponent<PlayerController>();
+		postProcessingBehaviour.enabled = playerController.InClouds;
 	}
 }
