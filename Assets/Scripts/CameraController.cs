b381c567ab62dc294d90b069872eb725f1f5b4f9
Phillip Schichtel <phillip.public@schich.tel>
2017-07-30 21:12:48 +0200

    hide clouds when in them

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,12 +9,14 @@ public class CameraController : MonoBehaviour
 	public GameObject player;
 	private PostProcessingBehaviour postProcessingBehaviour;
 	private Camera cam;
+	private int originalCullingMask;
 
 	private void Start()
 	{
 		postProcessingBehaviour = GetComponent<PostProcessingBehaviour>();
 		postProcessingBehaviour.enabled = true;
 		cam = GetComponent<Camera>();
+		originalCullingMask = cam.cullingMask;
 	}
 
 	void Update ()
@@ -23,5 +25,6 @@ public class CameraController : MonoBehaviour
 		transform.position = new Vector3(playerPos.x, playerPos.y + 10, playerPos.z);
 		var playerController = player.GetComponent<PlayerController>();
 		postProcessingBehaviour.enabled = playerController.InClouds;
+		cam.cullingMask = playerController.InClouds ? cam.cullingMask & ~(1 << 13) : originalCullingMask;
 	}
 }
