4789fa5c5ba2b2f833eff5b4da2b7cde795d1bc7
Anselm Brehme <development.anselm@breh.me>
2017-07-30 17:15:29 +0200

    follow me

---
+++ Assets/EnemyWeapon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponType
+{
+	LASER
+}
+
+public class EnemyWeapon : MonoBehaviour
+{
+
+	public WeaponType type;
+	public int range;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		var playerPos = GameObject.Find("Player").transform.position;
+		var distance = (playerPos - gameObject.transform.position).sqrMagnitude;
+		if (distance < range * range)
+		{
+			playerPos.y = gameObject.transform.position.y;
+			gameObject.transform.LookAt(playerPos);
+		}
+	}
+}
