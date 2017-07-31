25d5171e2eafe744c5ce09567c5c6b5d95f1e7d7
Jonas Dann <jonas.chr.dann@gmail.com>
2017-07-31 15:04:28 +0200

    Added exploded sprites to the bases

--- Assets/Scripts/Destroyable.cs
+++ Assets/Scripts/Destroyable.cs
@@ -4,6 +4,7 @@ public class Destroyable : MonoBehaviour
 {
     public int life = 25;
     public GameObject explosion;
+    public GameObject leftOver;
 
     public virtual void Hit(int amount)
     {
@@ -19,7 +20,12 @@ public class Destroyable : MonoBehaviour
         if (explosion != null)
         {
             Instantiate(explosion, gameObject.transform.position, Quaternion.Euler(-90,0,0)).GetComponent<Explosion>().explode();
-        }        
+        }
+        if (leftOver != null)
+        {
+            var instanceLeftOver = Instantiate(leftOver);
+            instanceLeftOver.transform.position = gameObject.transform.position;
+        }
         Destroy(gameObject);
     }
     
