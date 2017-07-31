4cdd8baab91a74807f50aec419c0de4427e61158
Phillip Schichtel <phillip.public@schich.tel>
2017-07-31 14:10:52 +0200

    show arrows towards targets

---
+++ Assets/Scripts/ArrowController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+
+	public GameObject target;
+
+	void Update ()
+	{
+		var targetPos = target.transform.position;
+//		targetPos.y = transform.position.y;
+		transform.LookAt(targetPos);
+	}
+}
