5293be73554932bdadb0541060c70a27e845bcb3
Jonas Dann <jonas.chr.dann@gmail.com>
2017-07-31 21:38:49 +0200

    Added first powerup

---
+++ Assets/Scripts/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseController : MonoBehaviour
+{
+	public GameObject weapons;
+	public GameObject powerUp;
+	public Vector3 position;
+
+	private bool spawned = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if (weapons.transform.childCount == 0 && !spawned)
+		{
+			var powerUp = Instantiate(this.powerUp);
+			spawned = true;
+		}
+	}
+}
