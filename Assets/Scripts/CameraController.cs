90fb0185f68619a55c16be9a02a6563d0ab81bdc
katharinaspinner <rahtainka@gmail.com>
2017-08-01 03:04:04 +0200

    finished distortion filter

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.PostProcessing;
 
@@ -8,6 +10,8 @@ public class CameraController : MonoBehaviour
 {
 	public GameObject player;
 	private PostProcessingBehaviour postProcessingBehaviour;
+	private float tdis;
+	private float tvol;
 
 	private void Start()
 	{
@@ -25,6 +29,7 @@ public class CameraController : MonoBehaviour
 		transform.position = new Vector3(playerPos.x, playerPos.y + 10, playerPos.z);
 		var playerController = player.GetComponent<PlayerController>();
 		postProcessingBehaviour.enabled = playerController.InClouds > 0 && playerController.InCleaner < 0;
+		setMusicFilter();
 	}
 
 	void setMusicFilter()
@@ -34,11 +39,33 @@ public class CameraController : MonoBehaviour
 		var filter = GetComponent<AudioDistortionFilter>();
 		if (playerController.InClouds > 0 && playerController.InCleaner < 0)
 		{
-			filter.distortionLevel = 0.6f;
+			tdis += Time.deltaTime / 5;
+			tvol -= Time.deltaTime / 5;
+			if (tdis > 1)
+			{
+				tdis = 1;
+			}
+			if (tvol < 0)
+			{
+				tvol = 0;
+			}
+			filter.distortionLevel = Mathf.Lerp(0f, 0.9f, tdis);
+			backgrundmusic.volume = Mathf.Lerp(0.15f, 1f, tvol);
 		}
 		else
 		{
-			filter.distortionLevel = 0;
+			tdis -= Time.deltaTime/3;
+			tvol += Time.deltaTime / 3;
+			if (tdis < 0)
+			{
+				tdis = 0;
+			}
+			if (tvol > 1)
+			{
+				tvol = 1;
+			}
+			filter.distortionLevel = Mathf.Lerp(0f, 0.9f, tdis);
+			backgrundmusic.volume = Mathf.Lerp(0.15f, 1f, tvol);
 		}
 	}
 }
